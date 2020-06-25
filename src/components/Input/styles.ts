6eb41d5c24bd5c66e4ddaf93e8400ca8092466e8
Guilherme Moraes <guii.moraes.93@gmail.com>
2020-06-25 20:37:27 -0300

    feat: styling input state

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,12 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background-color: #232129;
   border-radius: 10px;
   border: 2px solid #232129;
   padding: 16px;
   width: 100%;
-  color: #f4ede8;
+  color: #666360;
 
   display: flex;
   align-items: center;
@@ -15,18 +20,31 @@ export const Container = styled.div`
     margin-top: 8px;
   }
 
+  svg {
+    margin-right: 16px;
+  }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `};
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `};
+
   input {
     flex: 1;
     border: 0;
     background-color: transparent;
+    color: #f4ede8;
 
     &::placeholder {
       color: #666360;
     }
-  }
-
-  svg {
-    margin-right: 16px;
-    color: #666360;
   }
 `;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -8,12 +9,16 @@ import logo from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logo} alt="Logo GoBarber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
@@ -26,7 +31,7 @@ const SignUp: React.FC = () => {
           />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <a href="to-do">
           <FiArrowLeft /> Voltar para login
         </a>
