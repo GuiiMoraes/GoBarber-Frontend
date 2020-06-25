710eb6c21b885d624610811588302e3dd24ea7e5
Guilherme Moraes <guii.moraes.93@gmail.com>
2020-06-25 18:07:16 -0300

    create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import GlobalStyles from './styles/global';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello World</h1>
+    </>
+  );
 };
 
 export default App;
