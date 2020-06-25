20610bdaf8984b2d31138d3f02fcd2e24709c093
Guilherme Moraes <guii.moraes.93@gmail.com>
2020-06-25 19:04:00 -0300

    feat: create SignIn page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import GlobalStyles from './styles/global';
 
+import SignIn from './pages/SignIn';
+
 // import { Container } from './styles';
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyles />
-      <h1>Hello World</h1>
+      <SignIn />
     </>
   );
 };
