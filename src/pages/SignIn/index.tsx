7214195fbc4ec370a3a6dd09da08f8f33a44cd41
Guilherme Moraes <guii.moraes.93@gmail.com>
2020-06-25 19:46:41 -0300

    feat: integrate Unform to project

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -8,11 +9,15 @@ import logo from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Content>
         <img src={logo} alt="Logo GoBarber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu login</h1>
 
           <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
@@ -26,7 +31,7 @@ const SignIn: React.FC = () => {
           <Button type="submit">Entrar</Button>
 
           <a href="to-do">Esqueci minha senha</a>
-        </form>
+        </Form>
         <a href="to-do">
           <FiLogIn /> Criar conta
         </a>
