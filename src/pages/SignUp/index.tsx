96cc02503d9f038640f702bd043417d660cbcef3
Guilherme Moraes <guii.moraes.93@gmail.com>
2020-06-25 22:00:31 -0300

    feat: add yup validations to project

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -8,10 +9,33 @@ import Input from '../../components/Input';
 import logo from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
+interface FormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const SignUp: React.FC = () => {
-  function handleSubmit(data: any): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (formData: FormData) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('O nome é obrigatório'),
+        email: Yup.string()
+          .email('Digite um e-mail válido')
+          .required('O e-mail é obrigatório'),
+        password: Yup.string().min(
+          6,
+          'A senha precisa ter no mínimo de 6 dígitos'
+        ),
+      });
+
+      await schema.validate(formData, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
