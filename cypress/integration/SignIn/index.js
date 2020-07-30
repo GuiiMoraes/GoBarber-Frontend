7c267e63c50a3b234b702ccbc5d2272328a092d0
Guilherme Moraes <guii.moraes.93@gmail.com>
2020-07-30 19:53:35 -0300

    refactor: change login test

--- cypress/integration/SignIn/index.js
+++ cypress/integration/SignIn/index.js
@@ -6,17 +6,17 @@ describe('SignIn - Login', () => {
     cy.route('POST', '/sessions').as('sessions_route');
   });
 
-  it('should send the correct values in session request', () => {
+  it('should be able to access plataform with correct credentials', () => {
     cy.get('[data-cy=email-input]').type(Cypress.env('user_email'));
     cy.get('[data-cy=password-input]').type(Cypress.env('user_password'));
 
     cy.get('[data-cy=form-submit]').click();
 
     cy.wait(['@sessions_route']).then(xmlHttpResponse => {
-      expect(xmlHttpResponse.request.headers).to.have.property(
-        'Content-Type',
-        'application/json;charset=utf-8'
-      );
+      // Check the status of request
+      expect(xmlHttpResponse.status).to.eql(200);
+
+      // Check the if the request sent all expected datas
       expect(xmlHttpResponse.request.body).to.have.property(
         'email',
         Cypress.env('user_email')
@@ -25,6 +25,10 @@ describe('SignIn - Login', () => {
         'password',
         Cypress.env('user_password')
       );
+
+      // Check the if the response it's as expected
+      expect(xmlHttpResponse.response.body).to.have.property('user');
+      expect(xmlHttpResponse.response.body).to.have.property('token');
     });
   });
 
